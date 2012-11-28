85fd74d212246852f1a64f6c3e50291a82acbcf8
r1pp3rj4ck <attila.bukor@gmail.com>
2012-11-28 02:18:48 +0100

    added license, changed composer

--- LKTwigstringBundle.php
+++ LKTwigstringBundle.php
@@ -1,6 +1,10 @@
 <?php
+
 /*
- * (c) LaKrue <symfony@lakrue.com>
+ * This file is part of the TwigString bundle
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace LK\TwigstringBundle;
